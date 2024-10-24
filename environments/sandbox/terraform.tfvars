0ebdd0f84f188854df7ed9e913e67df9f79a300a
vaibhav mishra <v-vaibhav.mishra@circlek.com>
2024-10-24 15:58:04 -0600

    updated files

--- environments/sandbox/terraform.tfvars
+++ environments/sandbox/terraform.tfvars
@@ -3,7 +3,6 @@ location                         = "Canada Central"
 environment                      = "Development" # Specify the environment (dev, test, prod, etc.)
 subscription_id                  = "a1fd2c95-863c-4813-929e-e73460c55005"
 tenant_id                        = "deb87405-c3fd-48f8-af81-65ca6d8d8e57"
-user_assigned_identity_client_id = "1a9aba57-00ca-4abd-a665-60597275db77"
 
 
 ######################################################################
--- provider.tf
+++ provider.tf
@@ -1,13 +1,10 @@
+# Define the required provider for Azure
 provider "azurerm" {
   features {}
 
+  # Optionally, configure the subscription and tenant details
   subscription_id = var.subscription_id # Specify the subscription ID
   tenant_id       = var.tenant_id       # Optional tenant ID
-
-  use_msi = true  # Enables Managed Identity for authentication
-
-  #using User Assigned Managed Identity, specify the client ID
-  client_id = var.user_assigned_identity_client_id  # Replace with the variable storing your client ID
 }
 
 # Backend block with no variables
