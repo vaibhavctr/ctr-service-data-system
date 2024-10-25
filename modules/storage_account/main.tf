174342a72099c28298904d0f037680dfde24d5f9
vaibhav mishra <v-vaibhav.mishra@circlek.com>
2024-10-24 20:45:00 -0600

    updated

--- modules/storage_account/main.tf
+++ modules/storage_account/main.tf
@@ -12,7 +12,7 @@ resource "azurerm_storage_account" "storage" {
 }
 
 # Output the primary access key of the storage account
-output "storage_account_access_key" {
-  value     = azurerm_storage_account.storage.primary_access_key
-  sensitive = true # Mark as sensitive to avoid logging this value
-}
+#output "storage_account_access_key" {
+#  value     = azurerm_storage_account.storage.primary_access_key
+#  sensitive = true # Mark as sensitive to avoid logging this value
+#}
