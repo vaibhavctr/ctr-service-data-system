3181790e6227a8a51e04229d58172a7278fb8b57
vaibhav mishra <v-vaibhav.mishra@circlek.com>
2024-11-05 11:54:35 -0700

    updated

--- main.tf
+++ main.tf
@@ -1,11 +1,5 @@
 # Define the required Terraform version
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
   required_version = ">= 1.3.0"
 }
 
