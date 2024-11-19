1cb03788083f97bccd38516e51e2c5648ba5cf94
vaibhav mishra <v-vaibhav.mishra@circlek.com>
2024-11-19 10:43:52 -0700

    updated code

--- modules/key_vault/main.tf
+++ modules/key_vault/main.tf
@@ -5,13 +5,7 @@ resource "azurerm_key_vault" "key_vault" {
   sku_name            = "standard"
   tenant_id           = var.tenant_id
 
-
-# Enable purge protection as required by Azure policies
-purge_protection_enabled    = true
-
-# Set soft delete retention period (recommended: 90 days)
-soft_delete_retention_days = 90
-
+  soft_delete_retention_days = 7
 
   network_acls {
     default_action = "Allow"
@@ -23,23 +17,8 @@ soft_delete_retention_days = 90
   }
 }
 
-# Key Vault Access Policy for Key Permissions
-resource "azurerm_key_vault_access_policy" "key_access_policy" {
+resource "azurerm_key_vault_secret" "secret" {
+  name         = var.secret_name
+  value        = var.secret_value
   key_vault_id = azurerm_key_vault.key_vault.id
-  tenant_id    = var.tenant_id
-  object_id    = var.object_id # This should be the principal (user, app, or managed identity) needing access
-
-  key_permissions = [
-    "Get",
-    "List",
-    "Encrypt",
-    "Decrypt"
-  ]
 }
-
-# Example secret resource (optional, can be removed if secrets aren't required)
-#resource "azurerm_key_vault_secret" "secret" {
-#  name         = var.secret_name
-#  value        = var.secret_value
-#  key_vault_id = azurerm_key_vault.key_vault.id
-#}
