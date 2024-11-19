76137b14866eaff0a058ac1db6362c957d0bc345
vaibhav mishra <v-vaibhav.mishra@circlek.com>
2024-11-19 10:49:15 -0700

    updated code

--- main.tf
+++ main.tf
@@ -99,18 +99,3 @@ module "managed_identity" {
   # Define the scope for the role assignment
   scope = "/subscriptions/${var.subscription_id}/resourceGroups/${azurerm_resource_group.rg.name}"
 }
-# Call the Role Assignment module
-module "role_assignment" {
-  source               = "./modules/role_assignment"
-  scope                = azurerm_resource_group.rg.id         # Reference the resource group ID
-  role_definition_name = "Contributor"                        # Assign Contributor role
-  #principal_id         = var.user_assigned_identity_object_id # Pass the UAMI object ID
-}
-
-# Assign a role to an AAD group
-#module "aad_group_role_assignment" {
-#  source              = "./modules/role_assignment"
-#  principal_id        = var.aad_group_object_id                    # Pass the AAD group object ID
-#  role_definition_name = "Reader"
-#  scope               = azurerm_resource_group.rg.id             # Reference the resource group ID
-#}
--- modules/role_assignment/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "role_assignment_id" {
-  description = "The ID of the role assignment."
-  value       = azurerm_role_assignment.role_assignment.id
-}
